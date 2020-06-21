3606107c44a0e55298c6266fe5885b3d81431fd8
Minseok Cha <meloncha0205@gmail.com>
2020-06-21 23:40:53 +0900

    #6 ROUTING

---
+++ src/routes/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About(props) {
+	console.log(props)
+	return (
+		<div className="about__container">
+			<span>
+				About this page: I built it because I love movies.
+			</span>
+		</div>
+	)
+}
+
+export default About;
